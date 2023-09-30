df0df59b04887ffe6b9072017cb1c0b12b9e9915
ibnurizqiaramadan <dadanibnu61@gmail.com>
2023-09-30 15:47:18 +0700

    update layout

--- src/components/playlist/playlistItem.tsx
+++ src/components/playlist/playlistItem.tsx
@@ -1,9 +1,16 @@
 import './playlistItem.css';
 
+
 function PlaylistItem(props: any) {
   return (
     <div className='playlist-item' key={props.number}>
-      <p className={`items ${props.number}`}>{ +new Date() } - hello hehe {props.number}</p>
+      <div className='title-group'>
+        <img className='thumbnails' src="https://i3.ytimg.com/vi/MofaZRiwiOw/maxresdefault.jpg"></img>
+        <div>
+          <p className='title'>{+new Date()} - hello hehe {props.number}</p>
+          <p className={`artis ${props.number}`}>Artist { +new Date() }</p>
+        </div>
+      </div>
       <p className={`duration`}>04:04</p>
     </div>
   );
--- src/components/thumbnail/thumbnail.tsx
+++ src/components/thumbnail/thumbnail.tsx
@@ -4,10 +4,13 @@ function Thumbnail() {
   return (
     <div className='thumbnail'>
       <div className='track'>
-        <h1 className='track-title'>artis - title</h1>
-        <h4 className='track-author'>hello guys</h4>
+        <p className='track-title'>artis - title</p>
+        <p className='track-author'>hello guys</p>
       </div>
-      <img src="https://i3.ytimg.com/vi/MofaZRiwiOw/maxresdefault.jpg"></img>
+      {/* <img src="https://i3.ytimg.com/vi/MofaZRiwiOw/maxresdefault.jpg"></img> */}
+      <div className='blur'>
+        <img src="https://cdn.discordapp.com/attachments/752130537455484999/1157577384724271204/pawel-nolbert-4u2U8EO9OzY-unsplash.jpg"></img>
+      </div>
     </div>
   );
 }
